0b18520135b0012f1140e2b41c9e8125d5e92da3
DarrenBellew <islarf6546@gmail.com>
2018-03-28 13:58:27 +0100

    ObstacleManager

---
+++ app/src/main/java/com/gmail/islarf/myfirstgame_learning/Constants.java
@@ -0,0 +1,11 @@
+package com.gmail.islarf.myfirstgame_learning;
+
+/**
+ * Created by Islarf on 28/03/2018.
+ */
+
+public class Constants {
+
+    public static int SCREEN_WIDTH;
+    public static int SCREEN_HEIGHT;
+}
